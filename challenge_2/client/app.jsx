0478aef3237e428ea7ce178ade32f6d53f635591
Don Chen <donchen@Dons-MacBook-Pro.local>
2020-03-05 19:02:59 -0800

    CHALLENGE 2: basic environment setup

--- challenge_2/client/app.jsx
+++ challenge_2/client/app.jsx
@@ -1,2 +1,17 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import axios from 'axios';
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
+  render() {
+    return (
+      <div> set up </div>
+    )
+  }
+}
 
+ReactDom.render(<App/>, document.getElementById('app'));
