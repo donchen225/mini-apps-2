f7caef62045ddb25887d9aa02314a6e23acdd214
Don Chen <donchen@Dons-MacBook-Pro.local>
2020-03-06 19:59:45 -0800

    CHALLENGE 2: successfully able to retrieve data from api call

--- challenge_2/client/app.jsx
+++ challenge_2/client/app.jsx
@@ -5,11 +5,35 @@ import axios from 'axios';
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {}
+    this.state = {
+      start: '2019-03-05',
+      end: '2020-03-05',
+      data: {}
+    }
+  }
+  componentDidMount() {
+    this.getBitcoinData();
+  }
+  getBitcoinData() {
+    axios.get('/bitcoin', {
+      params: {
+        start: this.state.start,
+        end: this.state.end
+      }
+    })
+    .then((res) => {
+      console.log('data', res.data);
+      this.setState({
+        data: res.data.bpi
+      })
+    })
+    .catch((err) => {
+      console.log(err);
+    })
   }
   render() {
     return (
-      <div> set up </div>
+      <div> Hello </div>
     )
   }
 }
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,8 +1,28 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
+const axios = require('axios');
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.listen(port, () => console.log(`App listening on port ${port}!`));
+app.get('/bitcoin', (req, res) => {
+  const {start, end} = req.query;
+  axios.get('https://api.coindesk.com/v1/bpi/historical/close.json', {
+    params: {
+      start: start,
+      end: end
+    }
+  })
+  .then((response) => {
+    res.status(200).send(response.data);
+  })
+  .catch((err) => {
+    res.status(500).send(err);
+  })
+})
+
+app.listen(PORT, () => console.log(`App listening on port ${port}!`));
