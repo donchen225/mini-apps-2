7015e7a2396bc5654e5739b7294c8fac0bb80bac
Don Chen <donchen@Dons-MacBook-Pro.local>
2020-03-08 15:49:54 -0700

    CHALLENGE 2: refactor to use hooks

--- challenge_2/client/app.jsx
+++ challenge_2/client/app.jsx
@@ -1,63 +1,53 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import ReactDom from 'react-dom';
+
 import axios from 'axios';
 import Form from './Form.jsx';
 import BarGraph from './BarGraph.jsx';
 import LineGraph from './LineGraph.jsx';
 import PieChart from './PieChart.jsx';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: {},
-      graph: ''
-    }
-  }
-  componentDidMount() {
-    this.getBitcoinData();
-  }
-  getBitcoinData(start, end) {
-    console.log('successfully able to get bitcoin data');
+const App = () => {
+  const [data, setData] = useState({});
+  const [graph, setGraph] = useState('');
+
+  useEffect(() => {
+    getBitcoinData();
+  }, [])
+
+  const getBitcoinData = (start, end) => {
+    console.log('successful in get bitcoin data');
     axios.get('/bitcoin', {
-      params: {
-        start: start,
-        end: end
-      }
+      params:{ start: start, end: end }
     })
     .then((res) => {
       console.log('data', res.data);
-      this.setState({
-        data: res.data.bpi
-      })
+      setData(res.data.bpi);
     })
     .catch((err) => {
       console.log(err);
     })
   }
-  handleGraphChange(graph) {
-    console.log('graph is changed to', graph);
-    this.setState({
-      graph: graph
-    })
+
+  const handleGraphChange = (graph) => {
+    console.log('change to', graph);
+    setGraph(graph);
   }
 
-  render() {
-    return (
-      <div>
-        <h2> Cryptocurrency Charting Tool </h2>
+  return (
+    <div>
+      <h2> Cryptocurrency Charting Tool </h2>
 
-        <Form
-          getBitcoinData={this.getBitcoinData.bind(this)}
-          handleGraphChange={this.handleGraphChange.bind(this)}>
-        </Form>
+      <Form
+        getBitcoinData={getBitcoinData}
+        handleGraphChange={handleGraphChange}>
+      </Form>
 
-        {this.state.graph==='bar' && <BarGraph data={this.state.data}/>}
-        {this.state.graph==='line' && <LineGraph data={this.state.data}/>}
-        {this.state.graph==='pie' && <PieChart data={this.state.data}/>}
-      </div>
-    )
-  }
+      {graph==='bar' && <BarGraph data={data}/>}
+      {graph==='line' && <LineGraph data={data}/>}
+      {graph==='pie' && <PieChart data={data}/>}
+    </div>
+  )
 }
 
 ReactDom.render(<App/>, document.getElementById('app'));
